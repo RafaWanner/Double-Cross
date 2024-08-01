8c73ea322138cbe129c1cceca2a17914afadf98e
Danilus04 <danilogarciaba@hotmail.com>
2024-08-01 02:07:28 -0300

    uptade styles + devTools

--- devTools.js
+++ devTools.js
@@ -3,16 +3,18 @@ function identation(input) {
     
     const match = temp.match(/Restrict:\s*(.*?)(?:\n|$)/i);
     const restrictValue = match ? match[1].trim() : '';
+    const name = ((temp.slice(0, temp.search('\nMax LV: '))).replace(/\s+/g, ''))
 
-    var output = `name: "${temp.slice(0, temp.search('\nMax LV: '))}",
+    var output = `const ${name.charAt(0).toLowerCase() + name.slice(1)} = {
+    name: "${temp.slice(0, temp.search('\nMax LV'))}",
     power: "full",
-    maxLvl: ${temp.slice(temp.search('Max LV: ') + 8, temp.search(' Timing: '))},
-    timing: "${temp.slice(temp.search('Timing: ') + 8, temp.search('\nSkill: '))}",
-    skill: "${temp.slice(temp.search('Skill: ') + 7, temp.search(' DFCLTY: '))}",
-    difficulty: "${temp.slice(temp.search('DFCLTY: ') + 8, temp.search('\nTarget: '))}",
-    target: "${temp.slice(temp.search('Target: ') + 8, temp.search(' RNG: '))}",
-    range: "${temp.slice(temp.search('RNG: ') + 5, temp.search('\nEncroach: '))}",
-    encroach: ${temp.slice(temp.search('Encroach: ') + 10, temp.search(' Restrict: '))},
+    maxLvl: ${temp.slice(temp.search('Max LV') + 8, temp.search(' Timing'))},
+    timing: "${temp.slice(temp.search('Timing') + 8, temp.search('\nSkill'))}",
+    skill: "${temp.slice(temp.search('Skill') + 7, temp.search(' DFCLTY'))}",
+    difficulty: "${temp.slice(temp.search('DFCLTY') + 8, temp.search('\nTarget'))}",
+    target: "${temp.slice(temp.search('Target') + 8, temp.search(' RNG'))}",
+    range: "${temp.slice(temp.search('RNG') + 5, temp.search('\nEncroach'))}",
+    encroach: ${temp.slice(temp.search('Encroach') + 10, temp.search(' Restrict'))},
     permEcroach: 0,
     restrict: "${restrictValue}",
     lvl: 0,
@@ -22,16 +24,21 @@ function identation(input) {
     },
     description(){
         return \`${(((temp.slice(temp.search(/\.\n/) + 2)).replace(/\n/g, '')).trimEnd()).replace(/LV/g, '${this.lvl}')}\`
-    }`;
+    }
+};
+
+function get${name}() {
+    return ${name.charAt(0).toLowerCase() + name.slice(1)};
+};`;
 
     console.log(output);
 
     return output;
 }
 
-const buttonSubmit = document.querySelector('.buttonSubmit');
-const inputText = document.getElementById('inputText');
-const outputText = document.querySelector('.outputText');
+const buttonSubmit = document.querySelector('.button-submit');
+const inputText = document.getElementById('input-text');
+const outputText = document.querySelector('.output-text');
 
 (buttonSubmit === null) ? null : buttonSubmit.addEventListener('click', function() {
     (outputText === null) ? null : outputText.textContent = identation(inputText);
